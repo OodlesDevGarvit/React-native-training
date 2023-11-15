fca0fe5d39b4cdeec46e49aa4448e8c529544621
OodlesDevGarvit <garvit.bajaj@oodles.io>
2023-11-15 11:37:04 +0530

    tab structure

--- App.js
+++ App.js
@@ -1,52 +1,16 @@
 import React, { useEffect, useRef, useState } from "react";
 import { View, Text, StyleSheet, FlatList, Button } from "react-native"
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Header from "./Components/Header.js";
-import Home from "./Components/Home.js";
-import Login from "./Components/Login.js";
+
 
 const App = () => {
 
-  const Stack = createNativeStackNavigator();
-
-  const addBtn = ()=>{
-    console.warn("btn is called");
-  }
-
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{
-          headerStyle:{
-            backgroundColor:"blue",
-          },
-          headerTintColor:"orange",
-          headerTitleStyle:{
-            fontSize:25
-          }
-        }} >
-        <Stack.Screen name="Login" component={Login}  
-        options={{
-          headerTitle:()=><Button title="left" onPress={addBtn}/>,
-          headerRight:()=><Header />,
-          headerStyle:{
-            backgroundColor:"blue",
-          },
-          headerTintColor:"white",
-          headerTitleStyle:{
-            fontSize:40
-          }
-        }}  
-        />
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View>
 
+    </View>
   );
+
 }
-
-
-
 
 
 export default App;
