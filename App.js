edb502405ea293a0c4ec052e26ba20b7dd2bd118
OodlesDevGarvit <garvit.bajaj@oodles.io>
2023-11-21 20:47:56 +0530

    add Delete Api method

--- App.js
+++ App.js
@@ -1,105 +1,97 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet, Button, TextInput } from "react-native";
+import { View, Text, StyleSheet, Button, TextInput, TouchableOpacity } from "react-native";
 
 const App = () => {
 
-  const [name, setName] = useState("");
-  const [age, setAge] = useState("");
-  const [email, setEmail] = useState("");
-  const [nameError, setNameError] = useState("false");
-  const [ageError, setAgeError] = useState("false");
-  const [emailError, setEmailError] = useState("false");
+  const [data, setData] = useState([]);
 
-
-
-  const saveData = async () => {
-
-    if (!name) {
-      setNameError(true);
-    }else{
-      setNameError(false);
-    }
-
-    if(!age){
-      setAgeError(true);
-    }else{
-      setAgeError(false);
-    }
-
-    if(!email){
-      setEmailError(true);
-    }else{
-      setEmailError(false);
-    }
-
-    if (!name || !age || !email) {
-      return (false);
-    }
-
+  const getApiData = async () => {
     const url = "http://192.168.1.41:3000/users";
-    const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json"
-      },
-      body: JSON.stringify({ name, age, email })
-    });
-    let result = await res.json();
-    if (result) {
-      console.warn("Data is saved");
-    }
+    // const url = "https://dummyjson.com/products/1";
+
+    const res = await fetch(url);
+    const result = await res.json();
+
+    setData(result);
   }
 
+  const deleteUser = async (id) => {
+    const url = `http://192.168.1.41:3000/users/${id}`;
+    const res = await fetch(url,{
+      method: "delete"
+    });
+    const result = await res.json();
+    console.log(res,result,id);
+    if (result) {
+      console.warn("User deleted");
+      getApiData();
+    }
+    
+    
+    
+  }
 
+  useEffect(() => {
+    getApiData();
+  }, [])
 
 
   return (
 
     <View style={styles.main}>
 
+      <View style={styles.dataWrapper}>
+        <Text>Name</Text>
+        <Text>Age</Text>
+        <Text>Email</Text>
+        <Button title="Delete" />
+        <Button title="Update" />
+      </View>
+
+      {
+        data.length ?
+
+          // data.map((item) => (
+          //   <View style={styles.dataWrapper}>
+          //     <View sty><Text>{item.name}</Text></View>
+          //     <View> <Text>{item.age}</Text></View>
+          //     <View> <Text>{item.email}</Text></View>
+          //     <TouchableOpacity onPress={()=>console.warn("btn is deleted")}>
+          //     <View> <Text>Delete</Text></View>
+          //     </TouchableOpacity>
+          //     <TouchableOpacity onPress={()=>console.warn("btn is updated")}>
+          //     <View><Text>Update</Text></View> 
+          //     </TouchableOpacity>
+          //   </View>))
+
+          // <View>{
+          //  data?.map(item=>{
+          //   return(
+          //     <View>
+          //       <Text>{item?.name}</Text>
+          //     </View>
+          //   )
+          //  })
+          // }</View>
 
 
-      <Text style={{ fontSize: 20 }}>Post API with Input Field Data</Text>
+          data.map((item) => {
+            return (
+              <View style={styles.dataWrapper}>
+                <Text>{item.name}</Text>
+                <Text>{item.age}</Text>
+                <Text>{item.email}</Text>
+                <Button title="Delete" onPress={() => deleteUser(item.id)} />
+                <Button title="Update" />
 
-      <TextInput
-        placeholder="Enter your name"
-        style={styles.TextBox}
-        value={name}
-        onChangeText={(text) => setName(text)}
-      />
-      {nameError ?
-        <Text>please enter valid name</Text>
-        : null
+              </View>
+            )
+          })
+          : null
       }
-
-      <TextInput
-        placeholder="Enter your age"
-        style={styles.TextBox}
-        value={age}
-        onChangeText={(text) => setAge(text)}
-      />
-      {ageError ?
-        <Text>please enter valid age</Text>
-        : null
-      }
-
-      <TextInput
-        placeholder="Enter your email"
-        style={styles.TextBox}
-        value={email}
-        onChangeText={(text) => setEmail(text)}
-      />
-      {emailError ?
-        <Text>please enter valid email</Text>
-        : null
-      }
-
-
-      <Button title="save data" onPress={saveData} />
-
 
     </View>
-  );
+  )
 }
 
 
@@ -107,11 +99,16 @@ const styles = StyleSheet.create({
   main: {
     flex: 1,
   },
-  TextBox: {
-    borderColor: "skyblue",
-    borderWidth: 1,
-    margin: 20
+  dataWrapper: {
+    flexDirection: 'row',
+    justifyContent: "space-around",
+    backgroundColor: "orange",
+    margin: 5,
+    padding: 8
   }
+
+
+
 
 })
 
